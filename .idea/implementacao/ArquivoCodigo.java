b9268fe081b8edca547d4f63f884a67cb6ab794a
gusta <gustavorandiiii@gmail.com>
2025-09-24 22:39:19 -0300

    Implementação Inicial da classe Projeto

---
+++ .idea/implementacao/ArquivoCodigo.java
@@ -0,0 +1,2 @@
+public class ArquivoCodigo {
+}
---
+++ .idea/implementacao/Projeto.java
@@ -0,0 +1,24 @@
+import java.util.List;
+// conjunto de arquivos do cod. fonte
+public class Projeto {
+    private String nome;
+    private String caminho;
+    private String relatorio; // começando com um relátorio unico depois irei expandir para uma List
+    private ArquivoCodigo ListArquivo;
+
+
+
+
+    public void adicionarArquivo(ArquivoCodigo arquivo){
+        return;
+    }
+
+    public void analisar(){
+        return;
+    }
+
+    public void gerarRelatorio(){
+        return;
+    }
+
+}
